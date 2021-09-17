b3c6bfe1bb4a16ee1d6f72ebd7fea827c3e24536
Vismay-addweb <vismay@addwebsolution.in>
2021-09-17 17:09:21 +0530

    mergee

---
+++ src/components/List.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import * as type from './type'
+const List: React.FC<{data:type.data[],deletedata:(string:string)=>void}>  = (props) =>
+{
+
+    return(
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>name</th>
+                    <th>age</th>
+                    <th>delete</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            {props.data.map(data=> (
+            <tr key={Math.random()}>
+                <td>{data.uname}</td>
+                <td>{data.age} </td>
+                <td><button className='btn btn-danger' onClick={()=>props.deletedata(data.uname)}>delete</button></td>
+            </tr>
+            ))}
+            </tbody>
+        </table>
+        
+    )
+}
+
+export default List;
---
+++ src/components/type.tsx
@@ -0,0 +1,4 @@
+export type data = {
+    uname:string
+    age:number;
+}
